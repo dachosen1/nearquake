54d3bdc1913f85ca41bf528012957e78c1aa0570
Anderson Nelson <anderson.nelson1@gmail.com>
2023-05-21 14:19:09 -0700

    [BUG] Sytax Error  (#3)

--- nearquake/utils.py
+++ nearquake/utils.py
@@ -2,53 +2,78 @@ import logging
 import os
 from datetime import date, datetime, timedelta
 from httpx import post
-
-
 from psycopg2 import connect, sql
-
 import tweepy
-
-
 
 _logger = logging.getLogger(__name__)
 
 
-class DatabaseOperator:
+class DbOperator:
     """
-      Connects to and executes database SQL queries
+    Connects to and executes database SQL queries
     """
-    def __init__(self, host, user, password, dbname):
+
+    def __init__(self):
+        self.connection = None
+        self.cursor = None
+
+    def connect(self, host, user, password, dbname, port):
         """
-        :param host: Host name 
+        :param host: Host name
         :param user: User name
         :param password: Password
-        :param dbname: Database name 
+        :param dbname: Database name
         """
-        self.connection = connect(
-            host=host,
-            user=user,
-            password=password,
-            dbname=dbname,
-        )
-        self.cursor = self.connection.cursor()
 
-    def execute(self, query, mode='all'):
+        self.host = host
+        self.dbname = dbname
+        self.port = port
+
+        try:
+            self.connection = connect(
+                host=host, user=user, password=password, dbname=dbname, port=port
+            )
+            self.cursor = self.connection.cursor()
+        except Exception as e:
+            _logger.error(f"Failed to connect to the database: {e}")
+
+    def fetch(self, query, type="all"):
         """
         Executes an SQL query
 
-        :param query: SQL Query 
+        :param query: SQL Query
         :param mode: Specifies the type of query. Options are 'all' (retrieves all rows) and 'one' (returns a single row).
         Defaults to 'all'.
         """
+        try:
+            self.cursor.execute(sql.SQL(query))
+            if type == "all":
+                return self.cursor.fetchall()
+            elif type == "one":
+                return self.cursor.fetchone()
+            else:
+                raise ValueError("Invalid type. Expected 'all' or 'one'.")
 
-        self.cursor.execute(sql.SQL(query))
-        if mode=='all':
-            return self.cursor.fetchall()
-        elif mode == 'one':
-            return self.cursor.fetchone()
-        else:
-            raise ValueError("Invalid type. Expected 'all' or 'one'.")
+        except Exception as e:
+            print("Failed to execute fetch query: ", e)
 
+    def insert(self, query, data, mode):
+        """
+        Insert data into the database.
+
+        :param query: SQL Query
+        :param data: Data
+        :param mode: _description_
+        """
+        try:
+            if mode == "one":
+                return self.cursor.execute(sql.SQL(query), data)
+
+            elif mode == "all":
+                return self.cursor.executemany(sql.SQL(query), data)
+
+        except Exception as e:
+            print("Failed to execute insert query: ", e)
 
     def close(self):
         self.cursor.close()
@@ -57,26 +82,39 @@ class DatabaseOperator:
     def __enter__(self):
         return self
 
-    def __exit__(self):
-        self.close_connection()
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
 
 
 class QueryExecutor:
     """
-      Functions to run SQL queries
+    Functions to run SQL queries and exit the connection to the database
     """
+
     def __init__(self, database):
         self.database = database
 
-    def run_query(self, query, mode):
-        """_summary_
+    def fetch(self, query, mode):
+        """
+        Fetch data from a database and close the database connection.
 
-        :param query: SQL Query 
+        :param query: SQL Query
         :param mode: Specifies the type of query. Options are 'all' (retrieves all rows) and 'one' (returns a single row).
         Defaults to 'all'.
         """
         with self.database as db:
-            return db.execute(query, mode)
+            return db.fetch(query, mode)
+
+    def insert(self, query, data, mode):
+        """
+        Insert data into a database and close the database connection.
+
+        :param query: SQL Query
+        :param mode: Specifies the type of query. Options are 'all' (retrieves all rows) and 'one' (returns a single row).
+        Defaults to 'all'.
+        """
+        with self.database as db:
+            return db.insert(query, data, mode)
 
 
 def connect_db():
@@ -238,9 +276,7 @@ def weekly_top_tweet():
 
     tweet = f"Largest Quakes\nWeek = Mag: {mag_week}, location: {location_week} on {time_week.date()}\n Month = Mag: {mag_month}, location: {location_month} on {time_month.date()} \n YTD = Mag: {mag_ytd}, location: {location_ytd} on {time_ytd.date()}"
     post_tweet(tweet)
-    return (
-        'Done'
-    )
+    return "Done"
 
 
 def weekly_quake_count():
@@ -252,6 +288,5 @@ def weekly_quake_count():
 
     ytd = datetime.today().replace(day=1, month=1).date()
     count_ytd = count_quake(start=ytd, end=datetime.now().date())[0]
-    tweet =  f"--------- Total Earthquakes Greater than 5.0 ---------\nWeek = Count: {count_week[0]} \nMonth = Count {count_month[0]} \nYTD = Count: {count_ytd[0]} \n #earthquake"
+    tweet = f"--------- Total Earthquakes Greater than 5.0 ---------\nWeek = Count: {count_week[0]} \nMonth = Count {count_month[0]} \nYTD = Count: {count_ytd[0]} \n #earthquake"
     return post_tweet(tweet)
-
