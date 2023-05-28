418a840f343e9cb7461dc26942b056e0bb6061be
Anderson Nelson <anderson.nelson1@gmail.com>
2023-05-28 15:38:24 -0700

    add docstrings and description

--- nearquake/config.py
+++ nearquake/config.py
@@ -17,7 +17,16 @@ EARTH_QUAKE_FEATURES = (
 
 API_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 
-_custom_date_ = "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime={year}-{month}-01%2000:00:00&endtime={year}-{month}-31"
+EARTHQUAKE_URL_TEMPLATE  = "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime={year}-{month}-01%2000:00:00&endtime={year}-{month}-31"
 
-def generate_custom_date_url(year, month, _custom_date_= _custom_date_):
-    return _custom_date_.format(year=year, month=month)
+def generate_earthquake_url(year, month, url_template= EARTHQUAKE_URL_TEMPLATE ):
+    """
+    generate the URL for extracting earthquakes that occurred during a specific year and month.
+     
+    :param year: Year 
+    :param month: Month
+    :param url_template: URL template string that includes placeholders for 'year' and 'month', 
+                        defaults to EARTHQUAKE_URL_TEMPLATE
+    :return: The URL path for the earthquakes that happened during the specified month and year.
+    """
+    return url_template.format(year=year, month=month)
