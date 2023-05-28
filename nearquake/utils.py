5e551bcbefa8be2ce0f2c6938101d843547932d6
Anderson Nelson <anderson.nelson1@gmail.com>
2023-05-27 21:12:18 -0700

    remove already defined functions

--- nearquake/utils.py
+++ nearquake/utils.py
@@ -290,31 +290,3 @@ def weekly_quake_count():
     count_ytd = count_quake(start=ytd, end=datetime.now().date())[0]
     tweet = f"--------- Total Earthquakes Greater than 5.0 ---------\nWeek = Count: {count_week[0]} \nMonth = Count {count_month[0]} \nYTD = Count: {count_ytd[0]} \n #earthquake"
     return post_tweet(tweet)
-
-        
-def get_response(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return json.loads(response.text)
-    else:
-        return None
-
-
-def get_image_url(response):
-    try:
-        return response['properties']['products']['shakemap'][0]['contents']['download/pga.jpg']['url']
-    except KeyError:
-        return None
-
-def download_image(url, id_, directory='image'):
-    response = requests.get(url)
-    if response.status_code == 200:
-        os.makedirs(directory, exist_ok=True) 
-        with open(os.path.join(directory, f'{id_}.jpg'), 'wb') as f:
-            f.write(response.content)
-
-def extract_coordinates(data): 
-    coordinates = []
-    for d in data['features']:
-        coordinates.append([d['id'], *d['geometry']['coordinates']])
-    return coordinates
