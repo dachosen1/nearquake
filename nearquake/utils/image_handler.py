8117e8aa9882dd32aacf06b7d0ceed3fc6fa15bc
Anderson Nelson <anderson.nelson1@gmail.com>
2023-05-27 20:53:03 -0700

    update download image

--- nearquake/utils/image_handler.py
+++ nearquake/utils/image_handler.py
@@ -21,9 +21,26 @@ def get_quake_image_url(url):
         return None
 
 
-def download_image(url, id_, directory="image"):
-    response = requests.get(url)
-    if response.status_code == 200:
-        os.makedirs(directory, exist_ok=True)
-        with open(os.path.join(directory, f"{id_}.jpg"), "wb") as f:
+def download_image(url, id_, directory='image'):
+    """
+    Downloads an image from a given URL and saves it into a specified directory.
+    
+    :param url: URL of the image to download.
+    :param id_: Identifier to be used in the image file name.
+    :param directory: Directory where the image will be saved. Defaults to 'image'.
+    :return: None
+    """
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        print(f"Error: {err}")
+        return
+    
+    os.makedirs(directory, exist_ok=True) 
+
+    try:
+        with open(os.path.join(directory, f'{id_}.jpg'), 'wb') as f:
             f.write(response.content)
+    except Exception as e:
+        print(f"An error occurred while writing the file: {e}")
