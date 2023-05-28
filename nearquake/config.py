ec5bdd1a737db1a9c0edd1799c5c521997111f44
Anderson Nelson <anderson.nelson1@gmail.com>
2023-05-28 15:21:56 -0700

    update url load range

--- nearquake/config.py
+++ nearquake/config.py
@@ -16,3 +16,8 @@ EARTH_QUAKE_FEATURES = (
 )
 
 API_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+
+_custom_date_ = "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime={year}-{month}-01%2000:00:00&endtime={year}-{month}-31"
+
+def generate_custom_date_url(year, month, _custom_date_= _custom_date_):
+    return _custom_date_.format(year=year, month=month)
