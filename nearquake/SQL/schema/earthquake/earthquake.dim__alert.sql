94f40c417d019c9d51fd6488d33d5a38724f6f6b
Anderson Nelson <anderson.nelson1@gmail.com>
2023-07-01 17:28:54 -0700

    migrate event details and alert

--- nearquake/SQL/schema/earthquake/earthquake.dim__alert.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS earthquake.dim__alert (
-    id_alrert INTEGER PRIMARY KEY,
-    alert VARCHAR(50)
-);
-COMMENT ON COLUMN earthquake.dim__alert.id_alrert IS 'Primary key';
-COMMENT ON COLUMN earthquake.dim__alert.alert IS 'Alert level of the earthquake';
