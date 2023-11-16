8ce07b986a90ad1af54a4082bbd2961d41628437
dachosen1 <anderson.nelson1@gmail.com>
2023-11-15 21:49:52 -0500

    migrate earthquake schema

--- nearquake/SQL/schema/earthquake/earthquake.fct__upload_date.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS earthquake.fct__upload_date (
-    uploadid INTEGER PRIMARY KEY,
-    upload_date DATE,
-    max_quake_date DATE
-);
-COMMENT ON COLUMN earthquake.fct__upload_date.uploadid IS 'Unique id of the upload event ';
-COMMENT ON COLUMN earthquake.fct__upload_date.upload_date IS 'The date the upload event occured ';
-COMMENT ON COLUMN earthquake.fct__upload_date.max_quake_date IS 'The latest earthquake that occured on the upload date';
