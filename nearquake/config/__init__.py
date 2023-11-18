8c8d381de82287ed49ba65c0d8402ca7d43f624f
dachosen1 <anderson.nelson1@gmail.com>
2023-11-18 02:04:45 -0500

    reformat

--- nearquake/config/__init__.py
+++ nearquake/config/__init__.py
@@ -1,33 +1,36 @@
 from dataclasses import dataclass
 
 
+@dataclass
 class Config:
-    def __init__(self) -> None:
-        self.EARTH_QUAKE_FEATURES = (
-            "ids",
-            "mag",
-            "place",
-            "time",
-            "updated",
-            "tz",
-            "felt",
-            "cdi",
-            "mmi",
-            "alert",
-            "status",
-            "tsunami",
-            "type",
-            "title",
-        )
+    EARTH_QUAKE_FEATURES: tuple = (
+        "ids",
+        "mag",
+        "place",
+        "time",
+        "updated",
+        "tz",
+        "felt",
+        "cdi",
+        "mmi",
+        "alert",
+        "status",
+        "tsunami",
+        "type",
+        "title",
+    )
 
-        self.API_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_{time}.geojson"
+    API_URL: str = (
+        "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_{time}.geojson"
+    )
 
-        self.EARTHQUAKE_URL_TEMPLATE = "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime={year}-{month}-01%2000:00:00&endtime={year}-{month}-31"
+    EARTHQUAKE_URL_TEMPLATE: str = "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime={year}-{month}-01%2000:00:00&endtime={year}-{month}-31"
 
-    @dataclass
-    def generate_earthquake_url(year, month):
+    @staticmethod
+    def generate_earthquake_url(year: int, month: int) -> str:
         """
         Generate the URL for extracting earthquakes that occurred during a specific year and month.
+
         :param year: Year
         :param month: Month
         :return: The URL path for the earthquakes that happened during the specified month and year.
