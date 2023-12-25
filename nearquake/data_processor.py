0827423656af6e9857d2d79d67e1f6b59f32da7f
Anderson Nelson <anderson.nelson1@gmail.com>
2023-12-25 03:36:15 -0500

    refactor earquake class

---
+++ nearquake/data_processor.py
@@ -0,0 +1,96 @@
+import logging
+from datetime import datetime
+
+from nearquake.config import generate_time_range_url, ConnectionConfig
+from nearquake.utils.db_sessions import DbSessionManager
+from nearquake.app.db import EventDetails
+from tqdm import tqdm
+from nearquake.utils import (
+    fetch_json_data_from_url,
+    convert_timestamp_to_utc,
+    generate_date_range,
+)
+
+
+_logger = logging.getLogger(__name__)
+
+
+class Earthquake:
+    def extract_data_properties(self, url):
+        """_summary_
+
+        :param url: _description_
+        """
+        data = fetch_json_data_from_url(url=url)
+
+        conn = DbSessionManager(config=ConnectionConfig())
+        timestamp_now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+        added = 0
+        skipped = 0
+
+        with conn:
+            try:
+                for i in data["features"]:
+                    id_event = i["id"]
+                    properties = i["properties"]
+                    coordinates = i["geometry"]["coordinates"]
+
+                    timestamp_utc = convert_timestamp_to_utc(properties.get("updated"))
+                    time_stamp_date = timestamp_utc.date().strftime("%Y-%m-%d")
+
+                    earthquake_properties = {
+                        "id_event": id_event,
+                        "mag": properties.get("mag"),
+                        "ts_event_utc": timestamp_utc.strftime("%Y-%m-%d %H:%M:%S"),
+                        "ts_updated_utc": timestamp_now,
+                        "tz": properties.get("tz"),
+                        "felt": properties.get("felt"),
+                        "detail": properties.get("detail"),
+                        "cdi": properties.get("cdi"),
+                        "mmi": properties.get("mmi"),
+                        "status": properties.get("status"),
+                        "tsunami": properties.get("tsunami"),
+                        "type": properties.get("type"),
+                        "title": properties.get("title"),
+                        "date": time_stamp_date,
+                        "place": properties.get("place"),
+                        "longitude": coordinates[0],
+                        "latitude": coordinates[1],
+                    }
+
+                    fetched_records = conn.fetch(
+                        model=EventDetails, column="id_event", item=id_event
+                    )
+
+                    if len(fetched_records) == 0:
+                        property_items = EventDetails(**earthquake_properties)
+                        conn.insert(property_items)
+                        added += 1
+                    else:
+                        skipped += 1
+                _logger.info(
+                    f"Upload Complete for {time_stamp_date}. Added {added} records, and {skipped} records were already in the database"
+                )
+
+            except Exception as e:
+                _logger.error(f"Encountereed an unexpected error: {e}")
+
+    def backfill_data_properties(self, start_date: str, end_date: str):
+        """_summary_
+
+        :param start_date: _description_
+        :param end_date: _description_
+        """
+    
+        date_range = generate_date_range(start_date, end_date)
+        for year, month in date_range:
+            for day in range(1,32): 
+                url=generate_time_range_url(year=str(year).zfill(2), month=str(month).zfill(2), day=str(day).zfill(2))
+                self.extract_data_properties(url)
+
+        _logger.info(f"Completed the Backfill.. Horray :) ")
+
+
+if __name__ == "__main__":
+    test = Earthquake()
+    test.backfill_data_properties(start_date="2023-01-01", end_date="2023-09-01")
--- nearquake/utils/__init__.py
+++ nearquake/utils/__init__.py
@@ -1,3 +1,12 @@
+import requests
+import json
+import os
+import logging
+from datetime import datetime, timezone
+
+_logger = logging.getLogger(__name__)
+
+
 def extract_properties(data, keylist):
     table = str.maketrans("", "", ",'")
     return {
@@ -15,3 +24,109 @@ def extract_coordinates(data):
     for d in data["features"]:
         coordinates.append([d["id"], *d["geometry"]["coordinates"]])
     return coordinates
+
+
+def get_earthquake_image_url(url):
+    """
+    Extract the image URL from the results of the USA.gov earthquake API
+
+    :param url: URL to the earthquake data.
+    :return: String containing the URL to the image, or None if no image could be found.
+    """
+    response = requests.get(url, timeout=5)
+    if response.status_code != 200:
+        _logger.error(
+            "Failed to get data from URL %s. Status code: %s", url, response.status_code
+        )
+        return None
+
+    try:
+        data = json.loads(response.text)
+        image_url = data["properties"]["products"]["shakemap"][0]["contents"][
+            "download/pga.jpg"
+        ]["url"]
+        return image_url
+    except KeyError:
+        _logger.error("Could not find image URL in response data.")
+        return None
+
+
+def download_image(url, id_, directory="image"):
+    """
+    Downloads an image from a given URL and saves it into a specified directory.
+
+    :param url: URL of the image to download.
+    :param id_: Identifier to be used in the image file name.
+    :param directory: Directory where the image will be saved. Defaults to 'image'.
+    :return: None
+    """
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        print(f"Error: {err}")
+        return
+
+    os.makedirs(directory, exist_ok=True)
+
+    try:
+        with open(os.path.join(directory, f"{id_}.jpg"), "wb") as f:
+            f.write(response.content)
+    except Exception as e:
+        _logger.error("An error occured while writing the file: %e", e)
+
+
+def fetch_json_data_from_url(url):
+    """
+    Loads  data from a provided URL.
+
+    :param url: URL
+    """
+    response = requests.get(url, timeout=5)
+
+    if response.status_code != 200:
+        _logger.error(
+            "Failed to get data from URL %s. Status code: %s", url, response.status_code
+        )
+        return None
+    try:
+        response.raise_for_status()
+        return json.loads(response.text)
+    except json.JSONDecodeError:
+        _logger.exception("Could not find image URL in response data.")
+        return None
+
+
+def convert_timestamp_to_utc(timestamp):
+    # The given timestamp (assuming it's in milliseconds)
+    timestamp = timestamp
+
+    # Convert milliseconds to seconds
+    timestamp_seconds = timestamp / 1000
+
+    # Convert to datetime object in UTC
+    utc_datetime = datetime.fromtimestamp(timestamp_seconds, timezone.utc)
+
+    return utc_datetime
+
+
+def extract_year_and_month(date):
+    date = datetime.strptime(date, "%Y-%m-%d").date()
+    return date.year, date.month
+
+
+def generate_date_range(start_date, end_date):
+    start_year, start_month = extract_year_and_month(start_date)
+    end_year, end_month = extract_year_and_month(end_date)
+
+    date_list = []
+
+    for year in range(start_year, end_year + 1):
+        for month in range(1, 13):
+            if year == start_year and month >= start_month:
+                date_list.append([year, month])
+            elif year == end_year and month <= end_month:
+                date_list.append([year, month])
+            elif year > start_year and year <= end_year:
+                date_list.append([year, month])
+    return date_list
