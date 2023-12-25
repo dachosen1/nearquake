eea75fdc2c8ee9ea8dafdcf4c9f74825793046b7
Anderson Nelson <anderson.nelson1@gmail.com>
2023-12-25 03:42:16 -0500

    update test cases

--- nearquake/test/config_test.py
+++ nearquake/test/config_test.py
@@ -1,13 +1,13 @@
 import pytest
-from nearquake.config import QuakeConfig
+from nearquake.config import generate_time_range_url, generate_time_period
 
 
 def test_generate_time_range_url():
     year = 2021
     month = 5
     day = 10
-    expected_url = "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime=2021-5-10%2000:00:00&endtime=2021-5-10"
-    assert QuakeConfig.generate_time_range_url(year, month, day) == expected_url
+    expected_url = "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime=2021-5-10%2000:00:00&endtime=2021-5-10%2023:59:59"
+    assert generate_time_range_url(year, month, day) == expected_url
 
 
 def test_generate_time_period_day_url():
@@ -16,7 +16,7 @@ def test_generate_time_period_day_url():
     expected_url = (
         "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
     )
-    assert QuakeConfig.generate_time_period(time_period="day") == expected_url
+    assert generate_time_period(time_period="day") == expected_url
 
 
 def test_generate_time_period_week_url():
@@ -25,7 +25,7 @@ def test_generate_time_period_week_url():
     expected_url = (
         "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
     )
-    assert QuakeConfig.generate_time_period(time_period="week") == expected_url
+    assert generate_time_period(time_period="week") == expected_url
 
 
 def test_generate_time_period_month_url():
@@ -34,10 +34,10 @@ def test_generate_time_period_month_url():
     expected_url = (
         "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
     )
-    assert QuakeConfig.generate_time_period(time_period="month") == expected_url
+    assert generate_time_period(time_period="month") == expected_url
 
 
 def test_generate_period_url_error():
     """Test if ValueError is raised for invalid time periods with pytest"""
     with pytest.raises(ValueError):
-        QuakeConfig.generate_time_period(time_period="invalid_time_period")
+        generate_time_period(time_period="invalid_time_period")
