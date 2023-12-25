27ad39c47145d82b2aed4f3d3e8b7acf3ab96dea
Anderson Nelson <anderson.nelson1@gmail.com>
2023-12-25 03:35:51 -0500

    update config

--- nearquake/config/__init__.py
+++ nearquake/config/__init__.py
@@ -5,41 +5,40 @@ import os
 load_dotenv()
 
 
-@dataclass
-class QuakeConfig:
-    EARTH_QUAKE_FEATURES: tuple = (
-        "ids",
-        "mag",
-        "place",
-        "time",
-        "updated",
-        "tz",
-        "felt",
-        "cdi",
-        "mmi",
-        "alert",
-        "status",
-        "tsunami",
-        "type",
-        "title",
-    )
+API_BASE_URL: str = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_{time_period}.geojson"
 
-    API_URL: str = (
-        "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_{time}.geojson"
-    )
+EARTHQUAKE_URL_TEMPLATE: str = "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime={year}-{month}-{day}%2000:00:00&endtime={year}-{month}-{day}%2023:59:59"
 
-    EARTHQUAKE_URL_TEMPLATE: str = "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime={year}-{month}-01%2000:00:00&endtime={year}-{month}-31"
 
-    @staticmethod
-    def generate_earthquake_url(year: int, month: int) -> str:
-        """
-        Generate the URL for extracting earthquakes that occurred during a specific year and month.
+def generate_time_range_url(year: int, month: int, day: int) -> str:
+    """
+    Generate the URL for extracting earthquakes that occurred during a specific year and month.
 
-        :param year: Year
-        :param month: Month
-        :return: The URL path for the earthquakes that happened during the specified month and year.
-        """
-        return QuakeConfig.EARTHQUAKE_URL_TEMPLATE.format(year=year, month=month)
+    Example usage: generate_time_range_url('2018','01', '12')
+
+    :param year: Year
+    :param month: Month
+    :param day: Day
+    :return: The URL path for the earthquakes that happened during the specified month and year.
+    """
+    return EARTHQUAKE_URL_TEMPLATE.format(year=year, month=month, day=day)
+
+
+def generate_time_period(time_period: int) -> str:
+    """
+    Generate the URL for extracting earthquakes that occurred during a specific time.
+
+    :param time: The time period for the query. Options are 'day', 'week', 'month'.
+    :return: The URL path for the earthquakes that happened during the specified month and year.
+    """
+
+    valid_periods = {"day", "week", "month"}
+    if time_period not in valid_periods:
+        raise ValueError(
+            f"Invalid time period: {time_period}. Valid options are: {valid_periods}"
+        )
+
+    return API_BASE_URL.format(time_period=time_period)
 
 
 @dataclass
@@ -55,8 +54,3 @@ class ConnectionConfig:
         if self.sqlengine is None:
             raise ValueError("SQL Engine is not specifed")
         return f"{self.sqlengine}://{self.user}:{self.password}@{self.host}:{self.port}/{self.dbname}"
-
-
-if __name__ == "__main__":
-    config = ConnectionConfig()
-    config.generate_connection_url("postgresql")
