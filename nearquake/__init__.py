68fdbf235b106b06961ca3d5cc5be75dd53ada40
Anderson Nelson <anderson.nelson1@gmail.com>
2023-12-29 16:58:21 -0500

    update logging (#87)

--- nearquake/__init__.py
+++ nearquake/__init__.py
@@ -1,23 +1,47 @@
 import logging
+import logging.handlers
+import json
 import sys
 
-# --------------------------------------------------------------------------------- Logging
 
-FORMATTER = logging.Formatter(
-    "%(asctime)s — %(name)s — %(levelname)s —" "%(funcName)s:%(lineno)d — %(message)s"
-)
+# JSON Formatter
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        log_record = {
+            "time": self.formatTime(record, self.datefmt),
+            "level": record.levelname,
+            "name": record.name,
+            "message": record.getMessage(),
+            # Add more fields if necessary
+        }
+        return json.dumps(log_record)
 
 
-def get_console_handler():
+# Logging Configuration
+def setup_logging():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO if not DEBUG_MODE else logging.DEBUG)
+
+    # Console Handler
     console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(FORMATTER)
-    return console_handler
+    console_handler.setFormatter(
+        logging.Formatter("%(asctime)s — %(levelname)s — %(message)s")
+    )
+    logger.addHandler(console_handler)
+
+    # File Handler with Log Rotation
+    file_handler = logging.handlers.RotatingFileHandler(
+        "nearquake.log", maxBytes=1048576, backupCount=5
+    )
+    file_handler.setFormatter(JsonFormatter())
+    file_handler.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+
+    logger.propagate = False
 
 
-logging.basicConfig(level=logging.INFO)
+# Initialize logging
+DEBUG_MODE = True  # Set to False in production
 
+setup_logging()
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.addHandler(get_console_handler())
-
-logger.propagate = False
