c2a1526dd7d7cfb4749467489e05125d1dc8d3c2
Anderson Nelson <anderson.nelson1@gmail.com>
2024-01-02 14:23:20 -0500

    [Improvement] Added Improvements to enhance performance (#97)

--- main.py
+++ main.py
@@ -2,10 +2,29 @@ from nearquake.data_processor import Earthquake
 from nearquake.config import generate_time_period_url
 
 
-def daily_fetch():
-    test = Earthquake()
-    test.extract_data_properties(generate_time_period_url("day"))
-
-
 if __name__ == "__main__":
-    daily_fetch()
+    import argparse
+
+    run = Earthquake()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d", "--daily", action="store_true", help="Execute the daily program"
+    )
+    parser.add_argument(
+        "-w", "--weekly", action="store_true", help="Execute the weekly program"
+    )
+    parser.add_argument(
+        "-m", "--monthly", action="store_true", help="Execute the monthly program"
+    )
+
+    args = parser.parse_args()
+
+    if args.daily:
+        run.extract_data_properties(url=generate_time_period_url("day"))
+
+    if args.weekly:
+        run.extract_data_properties(url=generate_time_period_url("week"))
+
+    if args.monthly:
+        run.extract_data_properties(url=generate_time_period_url("month"))
--- nearquake/config/__init__.py
+++ nearquake/config/__init__.py
@@ -16,6 +16,27 @@ API_BASE_URL: str = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/a
 EARTHQUAKE_URL_TEMPLATE: str = "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime={year}-{month}-01%2000:00:00&endtime={year}-{month}-31%2023:59:59"
 
 
+@dataclass(kw_only=True)
+class QuakeFeatures:
+    id_event: str = field(default_factory=str)
+    mag: float = field(default_factory=float)
+    ts_event_utc: str = field(default_factory=str)
+    ts_updated_utc: str = field(default_factory=str)
+    tz: str = field(default_factory=str)
+    felt: str = field(default_factory=str)
+    detail: str = field(default_factory=str)
+    cdi: str = field(default_factory=str)
+    mmi: str = field(default_factory=str)
+    status: str = field(default_factory=str)
+    tsunami: str = field(default_factory=str)
+    type: str = field(default_factory=str)
+    title: str = field(default_factory=str)
+    date: str = field(default_factory=str)
+    place: str = field(default_factory=str)
+    longitude: float = field(default_factory=float)
+    latitude: float = field(default_factory=float)
+
+
 def generate_time_range_url(year: int, month: int) -> str:
     """
     Generate the URL for extracting earthquakes that occurred during a specific year and month.
@@ -24,7 +45,7 @@ def generate_time_range_url(year: int, month: int) -> str:
 
     :param year: Year
     :param month: Month
-    :param day: Day
+
     :return: The URL path for the earthquakes that happened during the specified month and year.
     """
     return EARTHQUAKE_URL_TEMPLATE.format(year=year, month=month)
@@ -52,7 +73,7 @@ def generate_time_period_url(time_period: int) -> str:
     return API_BASE_URL.format(time_period=time_period)
 
 
-@dataclass
+@dataclass()
 class ConnectionConfig:
     user: str = field(default_factory=lambda: os.environ.get("NEARQUAKE_USERNAME"))
     host: str = field(default_factory=lambda: os.environ.get("NEARQUAKE_HOST"))
