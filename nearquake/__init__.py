883e1a08acc836520ec83dbdfb5a967c48f673de
Anderson Nelson <anderson.nelson1@gmail.com>
2024-01-16 02:01:49 -0500

    [Logging] Update logging directory (#111)

--- nearquake/__init__.py
+++ nearquake/__init__.py
@@ -54,24 +54,11 @@ def setup_logging():
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO if not DEBUG_MODE else logging.DEBUG)
 
-    # Create directory path to save logs
-    create_dir("logs")
-
     # Console Handler with Standard Formatter
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.addFilter(FilterForHandler("console"))
     console_handler.setFormatter(CustomFormatter())
     logger.addHandler(console_handler)
-
-    # File Handler with Log Rotation and JSON Formatter
-    file_handler = logging.handlers.RotatingFileHandler(
-        f"logs/{TIMESTAMP_NOW}-nearquake.log", maxBytes=1048576, backupCount=5
-    )
-
-    file_handler.addFilter(FilterForHandler("file"))
-    file_handler.setFormatter(CustomFormatter())
-    file_handler.setLevel(logging.DEBUG)
-    logger.addHandler(file_handler)
 
     logger.propagate = False
 
